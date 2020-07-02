085b354788aa071886090b74a9eca7ca62c02f38
stone_d4 <stone_d4@outlook.com>
2020-07-02 22:27:49 +0500

    Add project files.

---
+++ AjaxCrudApp/Scripts/Scripts.js
@@ -0,0 +1,89 @@
+﻿function jQueryAjaxPost(form)
+{
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid())
+    {
+        $.ajax({
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    refreshAddNewTab($(form).attr('data-restUrl'), true);
+                    $.notify(response.message, "success");
+                    if (typeof activatejQueryTable !== 'undefined' && $.isFunction(activatejQueryTable))
+                        activatejQueryTable();
+                }
+                else
+                {
+                    $.notify(response.message, "error");
+                }
+                
+            } 
+        });
+    }
+    return false;
+}
+
+
+
+function refreshAddNewTab(resetUrl, showViewTab)
+{
+    $.ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Add New');
+            if (showViewTab)
+            $('ul.nav.nav-tabs a:eq(0)').tab('show');
+        }
+    });
+}
+
+
+
+function Edit(url)
+{
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Edit');
+            $('ul.nav.nav-tabs a:eq(1)').tab('show');
+        }
+    });
+}
+
+
+function Delete(url) {
+
+
+    if (confirm("Are you sure you want to do this?") == true)
+    {
+        $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    $.notify(response.message, "success");
+                    if (typeof activatejQueryTable !== 'undefined' && $.isFunction(activatejQueryTable))
+                        activatejQueryTable();
+                }
+                else
+                {
+                    $.notify(response.message, "error");
+                }
+
+
+            }
+        });
+    }
+}
+
+
